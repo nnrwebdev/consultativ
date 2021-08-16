db17c5d5e56d30b084a511a26babc74e5e98802f
Nagaraju <nnrwebdev@gmail.com>
2021-08-16 12:36:08 +0530

    logo changes

--- header.php
+++ header.php
@@ -36,7 +36,7 @@
         </div>
       </div>
       <nav class="navbar navbar-expand-lg navbar-light py-3 bg-white shadow-sm">
-        <div class="container"><a class="navbar-brand" href="index.php"><img src="img/logo.svg" alt="" width="200"></a>
+        <div class="container"><a class="navbar-brand" href="index.php"><img src="img/sample-logo.png" alt="" width="200"></a>
           <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
